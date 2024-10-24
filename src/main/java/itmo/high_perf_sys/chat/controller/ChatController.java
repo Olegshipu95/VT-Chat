ee3c9e047ad71dc6d2cdac0d806f27a12cae0b4a
Arhiiiip <rabokony@icloud.com>
2024-10-24 23:27:16 +0300

    feat: Added validation and catching exception

--- src/main/java/itmo/high_perf_sys/chat/controller/ChatController.java
+++ src/main/java/itmo/high_perf_sys/chat/controller/ChatController.java
@@ -5,18 +5,15 @@ import itmo.high_perf_sys.chat.dto.chat.response.ResponseGettingChats;
 import itmo.high_perf_sys.chat.dto.chat.response.ResponseGettingMessages;
 import itmo.high_perf_sys.chat.dto.chat.response.ResponseSearchChat;
 import itmo.high_perf_sys.chat.dto.chat.response.ResponseSearchMessage;
-import itmo.high_perf_sys.chat.dto.customer.request.CreateUserAccountRequest;
 import itmo.high_perf_sys.chat.service.ChatService;
-import itmo.high_perf_sys.chat.service.CustomerService;
+import itmo.high_perf_sys.chat.utils.ErrorMessages;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/chats")
@@ -29,46 +26,91 @@ public class ChatController {
     }
 
     @PostMapping("/chat/start")
-    public ResponseEntity<UUID> createChat(@RequestBody CreateChatRequest createChatRequest) {
-        chatService.createChat(createChatRequest);
-        return new ResponseEntity<>(UUID.randomUUID(), HttpStatus.OK);
+    public ResponseEntity<?> createChat(@Valid @RequestBody CreateChatRequest createChatRequest) {
+        Long idNewChat = chatService.createChat(createChatRequest);
+        return ResponseEntity.ok(idNewChat);
     }
 
     @GetMapping("/search")
-    public ResponseEntity<UUID> searchChat(@RequestParam(value = "userId") Long userId, @RequestParam(value = "request") String request,
-                                           @RequestParam(value = "pageNumber", required = false, defaultValue = "0") Long pageNumber,
-                                           @RequestParam(value = "countChatsOnPage", required = false, defaultValue = "20") Long countChatsOnPage) {
-        ResponseSearchChat response = chatService.searchChat(userId, request, pageNumber, countChatsOnPage);
-        return new ResponseEntity<>(UUID.randomUUID(), HttpStatus.OK);
+    public ResponseEntity<?> searchChat(@NotNull(message = ErrorMessages.ID_CANNOT_BE_NULL)
+                                        @Min(value = 0, message = ErrorMessages.ID_CANNOT_BE_NEGATIVE)
+                                        @RequestParam(value = "userId") Long userId,
+                                        @NotNull @RequestParam(value = "request") String request,
+                                        @RequestParam(value = "pageNumber", required = false, defaultValue = "0") Long pageNumber,
+                                        @RequestParam(value = "countChatsOnPage", required = false, defaultValue = "20") Long countChatsOnPage) {
+        try {
+            ResponseSearchChat response = chatService.searchChat(userId, request, pageNumber, countChatsOnPage);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
     }
 
     @GetMapping("/{chatId}/search")
-    public ResponseEntity<UUID> searchMessage(@PathVariable Long chatId, @RequestParam(value = "request") String request,
-                                              @RequestParam(value = "pageNumber", required = false, defaultValue = "0") Long pageNumber,
-                                              @RequestParam(value = "countMessagesOnPage", required = false, defaultValue = "20") Long countMessagesOnPage) {
-        ResponseSearchMessage response = chatService.searchMessage(chatId, request, pageNumber, countMessagesOnPage);
-        return new ResponseEntity<>(UUID.randomUUID(), HttpStatus.OK);
+    public ResponseEntity<?> searchMessage(@NotNull(message = ErrorMessages.ID_CANNOT_BE_NULL)
+                                           @Min(value = 0, message = ErrorMessages.ID_CANNOT_BE_NEGATIVE)
+                                           @PathVariable Long chatId,
+                                           @NotNull(message = ErrorMessages.REQUEST_CANNOT_BE_NULL)
+                                           @RequestParam(value = "request") String request,
+                                           @NotNull(message = ErrorMessages.PAGE_CANNOT_BE_NULL)
+                                           @Min(value = 0, message = ErrorMessages.PAGE_CANNOT_BE_NEGATIVE)
+                                           @RequestParam(value = "pageNumber", required = false, defaultValue = "0") Long pageNumber,
+                                           @NotNull(message = ErrorMessages.COUNT_PAGE_CANNOT_BE_NULL)
+                                           @Min(value = 0, message = ErrorMessages.COUNT_PAGE_CANNOT_BE_NEGATIVE)
+                                           @RequestParam(value = "countMessagesOnPage", required = false, defaultValue = "20") Long countMessagesOnPage) {
+        try {
+            ResponseSearchMessage response = chatService.searchMessage(chatId, request, pageNumber, countMessagesOnPage);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
     }
 
     @DeleteMapping("/chat/{chatId}")
-    public ResponseEntity<UUID> deleteChat(@PathVariable Long chatId) {
-        chatService.deleteChat(chatId);
-        return new ResponseEntity<>(UUID.randomUUID(), HttpStatus.OK);
+    public ResponseEntity<?> deleteChat(@NotNull(message = ErrorMessages.ID_CANNOT_BE_NULL)
+                                        @Min(value = 0, message = ErrorMessages.ID_CANNOT_BE_NEGATIVE)
+                                        @PathVariable Long chatId) {
+        try {
+            chatService.deleteChat(chatId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<UUID> getAllChatsByUserId(@PathVariable Long userId,
-                                                    @RequestParam(value = "pageNumber", required = false, defaultValue = "0") Long pageNumber,
-                                                    @RequestParam(value = "countChatsOnPage", required = false, defaultValue = "20") Long countChatsOnPage) {
-        ResponseGettingChats response = chatService.getAllChatsByUserId(userId, pageNumber, countChatsOnPage);
-        return new ResponseEntity<>(UUID.randomUUID(), HttpStatus.OK);
+    public ResponseEntity<?> getAllChatsByUserId(@NotNull(message = ErrorMessages.ID_CANNOT_BE_NULL)
+                                                 @Min(value = 0, message = ErrorMessages.ID_CANNOT_BE_NEGATIVE)
+                                                 @PathVariable Long userId,
+                                                 @NotNull(message = ErrorMessages.PAGE_CANNOT_BE_NULL)
+                                                 @Min(value = 0, message = ErrorMessages.PAGE_CANNOT_BE_NEGATIVE)
+                                                 @RequestParam(value = "pageNumber", required = false, defaultValue = "0") Long pageNumber,
+                                                 @NotNull(message = ErrorMessages.COUNT_PAGE_CANNOT_BE_NULL)
+                                                 @Min(value = 0, message = ErrorMessages.COUNT_PAGE_CANNOT_BE_NEGATIVE)
+                                                 @RequestParam(value = "countChatsOnPage", required = false, defaultValue = "20") Long countChatsOnPage) {
+        try {
+            ResponseGettingChats response = chatService.getAllChatsByUserId(userId, pageNumber, countChatsOnPage);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
     }
 
     @GetMapping("/{chatId}")
-    public ResponseEntity<UUID> getAllMessagesByChatId(@PathVariable Long chatId,
-                                                       @RequestParam(value = "pageNumber", required = false, defaultValue = "0") Long pageNumber,
-                                                       @RequestParam(value = "countMessagesOnPage", required = false, defaultValue = "20") Long countMessagesOnPage) {
-        ResponseGettingMessages response = chatService.getAllMessagesByChatId(chatId, pageNumber, countMessagesOnPage);
-        return new ResponseEntity<>(UUID.randomUUID(), HttpStatus.OK);
+    public ResponseEntity<?> getAllMessagesByChatId(@NotNull(message = ErrorMessages.ID_CANNOT_BE_NULL)
+                                                    @Min(value = 0, message = ErrorMessages.ID_CANNOT_BE_NEGATIVE)
+                                                    @PathVariable Long chatId,
+                                                    @NotNull(message = ErrorMessages.PAGE_CANNOT_BE_NULL)
+                                                    @Min(value = 0, message = ErrorMessages.PAGE_CANNOT_BE_NEGATIVE)
+                                                    @RequestParam(value = "pageNumber", required = false, defaultValue = "0") Long pageNumber,
+                                                    @NotNull(message = ErrorMessages.COUNT_PAGE_CANNOT_BE_NULL)
+                                                    @Min(value = 0, message = ErrorMessages.COUNT_PAGE_CANNOT_BE_NEGATIVE)
+                                                    @RequestParam(value = "countMessagesOnPage", required = false, defaultValue = "20") Long countMessagesOnPage) {
+        try {
+            ResponseGettingMessages response = chatService.getAllMessagesByChatId(chatId, pageNumber, countMessagesOnPage);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
     }
 }
