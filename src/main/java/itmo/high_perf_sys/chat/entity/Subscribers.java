45a13fb09c8cea60fd3eef6d614cee4a4440223a
Олег Шипулин <olegshipu95@gmail.com>
2024-10-28 22:48:50 +0300

    fix: change schema for subs

--- src/main/java/itmo/high_perf_sys/chat/entity/Subscribers.java
+++ src/main/java/itmo/high_perf_sys/chat/entity/Subscribers.java
@@ -1,18 +1,41 @@
 package itmo.high_perf_sys.chat.entity;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
+
+import itmo.high_perf_sys.chat.utils.ErrorMessages;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "subscribers")
 public class Subscribers {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @NotNull(message = ErrorMessages.ID_CANNOT_BE_NULL)
+    @Min(value = 0, message = ErrorMessages.ID_CANNOT_BE_NEGATIVE)
+    private UUID id;
+
     @Column(name = "user_id")
-    private Long userId;
-    @Lob
-    @Column(name = "subscribers_ids")
-    private List<Long> subscribers;
+    @NotNull(message = ErrorMessages.ID_CANNOT_BE_NULL)
+    @Min(value = 0, message = ErrorMessages.ID_CANNOT_BE_NEGATIVE)
+    private UUID userId;
+
+    @Column(name = "subscribed_user_id")
+    @NotNull(message = ErrorMessages.ID_CANNOT_BE_NULL)
+    @Min(value = 0, message = ErrorMessages.ID_CANNOT_BE_NEGATIVE)
+    private UUID subscribedUserId;
+
+    @Column(name = "subscription_time")
+    @NotNull(message = "subscription time can not be null")
+    private LocalDateTime subscriptionTime;
 }
