d8245ee64820d4b58e389215d9a36e8c0c5a76ff
cemetiere <kirodron@mail.ru>
2024-10-28 22:52:22 +0300

    fix: post for response fix

--- src/main/java/itmo/high_perf_sys/chat/dto/feed/response/PostForResponse.java
+++ src/main/java/itmo/high_perf_sys/chat/dto/feed/response/PostForResponse.java
@@ -1,9 +1,13 @@
 package itmo.high_perf_sys.chat.dto.feed.response;
 import itmo.high_perf_sys.chat.entity.Post;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.sql.Timestamp;
 import java.util.UUID;
 
+@Getter
+@Setter
 public class PostForResponse{
     private UUID id;
     private UUID userId;
