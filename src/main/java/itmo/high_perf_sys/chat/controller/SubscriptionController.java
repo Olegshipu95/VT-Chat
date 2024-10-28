65bd83a3eb4926bd8c5ff9a2b56f028193b2a47d
Олег Шипулин <olegshipu95@gmail.com>
2024-10-28 22:08:54 +0300

    feat: write base for subs

---
+++ src/main/java/itmo/high_perf_sys/chat/controller/SubscriptionController.java
@@ -0,0 +1,29 @@
+package itmo.high_perf_sys.chat.controller;
+
+import itmo.high_perf_sys.chat.dto.customer.request.CreateSubRequest;
+import itmo.high_perf_sys.chat.dto.customer.request.UpdateUserInfoRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/subscribe")
+public class SubscriptionController {
+
+
+    @PostMapping
+    public ResponseEntity<UUID> makeSubscription(@RequestBody @Valid CreateSubRequest request){
+
+        var savedId = service.createSub(request);
+
+        return new ResponseEntity<>(savedId, HttpStatus.LOCKED);
+    }
+
+    @GetMapping
+    public ResponseEntity<UpdateUserInfoRequest> getSubscriptions() {
+        return ResponseEntity.noContent().build();
+    }
+}
