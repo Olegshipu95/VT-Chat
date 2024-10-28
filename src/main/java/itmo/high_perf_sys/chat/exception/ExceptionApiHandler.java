3867588c4aaea25d3ffeff2d1689569dc7137bce
Олег Шипулин <olegshipu95@gmail.com>
2024-10-28 19:45:05 +0300

    feat: add exception api handler

--- src/main/java/itmo/high_perf_sys/chat/exception/ExceptionApiHandler.java
+++ src/main/java/itmo/high_perf_sys/chat/exception/ExceptionApiHandler.java
@@ -1,4 +1,29 @@
 package itmo.high_perf_sys.chat.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.support.ErrorMessage;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
 public class ExceptionApiHandler {
+
+    @ExceptionHandler(UserAccountNotFoundException.class)
+    public ResponseEntity<ErrorMessage> handleUserAccountNotFound(UserAccountNotFoundException ex) {
+        return handleException(ex);
+    }
+
+    @ExceptionHandler(UserAccountWasNotInsertException.class)
+    public ResponseEntity<ErrorMessage> handleUserAccountWasNotInserted(UserAccountWasNotInsertException ex) {
+        return handleException(ex);
+    }
+
+    private ResponseEntity<ErrorMessage> handleException(Exception ex) {
+        log.info("Bad_Request: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorMessage(ex));
+    }
 }
