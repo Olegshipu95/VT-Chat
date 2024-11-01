d0d76fe9ae5557d3a2acd8ff613565109199fde8
Arhiiiip <rabokony@icloud.com>
2024-11-01 14:21:59 +0300

    feat: Added UsersChatsService

---
+++ src/main/java/itmo/high_perf_sys/chat/service/UsersChatsService.java
@@ -0,0 +1,41 @@
+package itmo.high_perf_sys.chat.service;
+
+import itmo.high_perf_sys.chat.entity.UsersChats;
+import itmo.high_perf_sys.chat.repository.chat.UsersChatsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UsersChatsService {
+
+    private final UsersChatsRepository usersChatsRepository;
+
+    @Autowired
+    public UsersChatsService(UsersChatsRepository usersChatsRepository) {
+        this.usersChatsRepository = usersChatsRepository;
+    }
+
+    public Optional<UsersChats> findByUserId(UUID uuid) {
+        return usersChatsRepository.findByUserId(uuid);
+    }
+
+    public void save(UsersChats usersChats) {
+        usersChatsRepository.save(usersChats);
+    }
+
+    public List<UUID> findIdsByChatId(UUID uuid) {
+        return usersChatsRepository.findIdsByChatId(uuid);
+    }
+
+    public Optional<UsersChats> findById(UUID uuid) {
+        return usersChatsRepository.findById(uuid);
+    }
+
+    public int countByChatId(UUID uuid) {
+        return usersChatsRepository.countByChatId(uuid);
+    }
+}
