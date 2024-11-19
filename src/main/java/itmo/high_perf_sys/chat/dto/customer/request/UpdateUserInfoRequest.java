969c37e1c5d6ee6b69cc59c367b8e5bdecdcedf8
Arhiiiip <rabokony@icloud.com>
2024-11-19 13:12:59 +0300

    fix: fixed naming in json request

--- src/main/java/itmo/high_perf_sys/chat/dto/customer/request/UpdateUserInfoRequest.java
+++ src/main/java/itmo/high_perf_sys/chat/dto/customer/request/UpdateUserInfoRequest.java
@@ -1,5 +1,6 @@
 package itmo.high_perf_sys.chat.dto.customer.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import itmo.high_perf_sys.chat.utils.ErrorMessages;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -9,12 +10,16 @@ import java.util.UUID;
 
 public record UpdateUserInfoRequest(
         @NotNull(message = ErrorMessages.ID_CANNOT_BE_NULL)
+        @JsonProperty("user_id")
         UUID userId,
         @NotBlank(message = "Name can't be blank")
+        @JsonProperty("name")
         String name,
         @NotBlank(message = "Surname can't be blank")
+        @JsonProperty("surname")
         String surname,
         @NotBlank(message = "Email can't be blank")
+        @JsonProperty("email")
         String email,
         String briefDescription,
         String city,
