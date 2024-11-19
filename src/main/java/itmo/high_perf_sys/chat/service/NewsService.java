42a629e88d9f09e792a041c2dde6c591ec7c7ddf
cemetiere <kirodron@mail.ru>
2024-11-19 13:13:54 +0300

    fix: news service fix

--- src/main/java/itmo/high_perf_sys/chat/service/NewsService.java
+++ src/main/java/itmo/high_perf_sys/chat/service/NewsService.java
@@ -9,12 +9,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
 public class NewsService {
-    private NewsRepository newsRepository;
-    public FeedResponse getPostsBySubscriber(Long userId, Long page, Long count) {
+    private final NewsRepository newsRepository;
+    public NewsService(NewsRepository newsRepository){
+        this.newsRepository = newsRepository;
+    }
+    public FeedResponse getPostsBySubscriber(UUID userId, Long page, Long count) {
         try {
             Page<Post> pageOfPosts = newsRepository.findPostsBySubscriber(userId, PageRequest.of(page.intValue(), count.intValue()));
             return new FeedResponse(
