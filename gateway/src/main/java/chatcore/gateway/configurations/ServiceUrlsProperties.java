b55a95942f5b6142c8c3cd3d2745b46192d9f33b
Олег Шипулин <olegshipu95@gmail.com>
2024-12-21 02:25:11 +0300

    security: add security and write navigation

--- gateway/src/main/java/chatcore/gateway/configurations/ServiceUrlsProperties.java
+++ gateway/src/main/java/chatcore/gateway/configurations/ServiceUrlsProperties.java
@@ -2,7 +2,6 @@ package chatcore.gateway.configurations;
 
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
 
 @Getter
 @ConfigurationProperties(prefix = "spring.services.urls")
