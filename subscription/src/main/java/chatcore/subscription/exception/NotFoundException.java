aa8d64cc431c6c6c8bdd7acc8b4256dc5fda5132
Arhiiiip <rabokony@icloud.com>
2024-12-21 16:55:03 +0300

    feat: added subscription microservice

---
+++ subscription/src/main/java/chatcore/subscription/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package chatcore.subscription.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ subscription/src/main/java/chatcore/subscription/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package chatcore.subscription.repository;
+
+
+import chatcore.subscription.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
