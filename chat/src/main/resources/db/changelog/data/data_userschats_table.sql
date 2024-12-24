de3a79304b3f5e716105408897907ebca87bd1a6
Arhiiiip <rabokony@icloud.com>
2024-12-24 18:41:36 +0300

    feat: added scripts for insert data in tables

---
+++ chat/src/main/resources/db/changelog/data/data_userschats_table.sql
@@ -0,0 +1,13 @@
+INSERT INTO users_chats (id, user_id) VALUES
+                                          ('users-chats-uuid1', 'uuid1'),
+                                          ('users-chats-uuid2', 'uuid2'),
+                                          ('users-chats-uuid3', 'uuid3'),
+                                          ('users-chats-uuid4', 'uuid4'),
+                                          ('users-chats-uuid5', 'uuid5');
+
+INSERT INTO users_chats_chats (users_chats_id, chats_ids) VALUES
+                                                              ('users-chats-uuid1', 'chat-uuid1'),
+                                                              ('users-chats-uuid2', 'chat-uuid2'),
+                                                              ('users-chats-uuid3', 'chat-uuid3'),
+                                                              ('users-chats-uuid4', 'chat-uuid4'),
+                                                              ('users-chats-uuid5', 'chat-uuid5');
