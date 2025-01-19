75ece9f59c026704a373efff9ec7546323f3c981
Arhiiiip <rabokony@icloud.com>
2025-01-19 14:29:18 +0300

    fix: try to fix feign

--- chat-user/src/main/java/user/repository/UserRepository.java
+++ chat-user/src/main/java/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package user.repository;
 
+import org.springframework.stereotype.Repository;
 import user.entity.User;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     @Query(value = """
