6df2cef500ffa0a0cc4815826e737b94cc8805f4
Олег Шипулин <olegshipu95@gmail.com>
2025-01-19 22:43:34 +0300

    fix feed

--- chat-feed/src/main/resources/data/data_post_table.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO posts (id, user_id, title, text, images, posted_time) VALUES
-                                                                      ('post-uuid1', 'uuid1', 'First Post', 'This is the first post.', NULL, '2023-10-01 10:00:00'),
-                                                                      ('post-uuid2', 'uuid2', 'Second Post', 'This is the second post.', NULL, '2023-10-02 11:00:00'),
-                                                                      ('post-uuid3', 'uuid3', 'Third Post', 'This is the third post.', NULL, '2023-10-03 12:00:00'),
-                                                                      ('post-uuid4', 'uuid4', 'Fourth Post', 'This is the fourth post.', NULL, '2023-10-04 13:00:00'),
-                                                                      ('post-uuid5', 'uuid5', 'Fifth Post', 'This is the fifth post.', NULL, '2023-10-05 14:00:00');
