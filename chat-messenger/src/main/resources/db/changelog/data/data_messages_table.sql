72e4a0f2cdd7d47269f7b2e432e256e7937fd7ab
arhip <rabokony@icloud.com>
2025-01-23 17:55:08 +0300

    fix: fixed start chat-messenger and chat-subscription

--- chat-messenger/src/main/resources/db/changelog/data/data_messages_table.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO messages (id, chat_id, author_id, text, messaged_time, photo) VALUES
-                                                                              ('message-uuid1', 'chat-uuid1', 'uuid1', 'Hello everyone!', '2023-10-01 10:00:00', NULL),
-                                                                              ('message-uuid2', 'chat-uuid2', 'uuid2', 'Hi there!', '2023-10-02 11:00:00', NULL),
-                                                                              ('message-uuid3', 'chat-uuid3', 'uuid3', 'Good morning!', '2023-10-03 12:00:00', NULL),
-                                                                              ('message-uuid4', 'chat-uuid4', 'uuid4', 'How are you?', '2023-10-04 13:00:00', NULL),
-                                                                              ('message-uuid5', 'chat-uuid5', 'uuid5', 'What\'s up?', '2023-10-05 14:00:00', NULL);
